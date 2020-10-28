650e00f2ce03d81f942671050e5a496c91ce82c8
MONA007 <1175104595@qq.com>
2020-10-28 16:06:26 +0800

    学习ing

--- mpl_squares.py
+++ mpl_squares.py
@@ -5,3 +5,4 @@ square = [1, 4, 9, 16, 25]
 plt.plot(square)
 
 plt.show()
+print('mk')
---
+++ test.py
@@ -0,0 +1,18 @@
+def is_palindrome(n):
+    str1 = str(n)
+    len1 = len(str1)-1
+    i = 0
+    while(i <= len1):
+        if(str1[i] != str1[len1]):
+            return False
+        i += 1
+        len1 -= 1
+    return True
+
+
+output = filter(is_palindrome, range(1, 1000))
+print('1~1000:', list(output))
+if list(filter(is_palindrome, range(1, 200))) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191]:
+    print('测试成功!')
+else:
+    print('测试失败!')
